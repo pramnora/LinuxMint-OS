661a131ebb9e1117581ebd55bd6d36afb9a93e95
Paul Ramnora <pramnora@yahoo.com>
2026-07-10 10:20:24 +0100

    Clarify date command output in date.sh
    Updated comment to clarify date command output.

--- bash/time-based/date.sh
+++ bash/time-based/date.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# display what is the current date/time
-
-date # output: Fri 10 Jul 04:24:05 BST 2026
+date # outputs what is the current date/time, eg.: Fri 10 Jul 04:24:05 BST 2026
